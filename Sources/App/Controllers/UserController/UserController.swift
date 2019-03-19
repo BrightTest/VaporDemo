afdbc03486aa3bedc8f6f2ba06f9a351f945cd3f
BrightTest <zl0930c10@163.com>
2019-03-19 22:42:20 +0800

    update UserCntroller

--- Sources/App/Controllers/UserController/UserController.swift
+++ Sources/App/Controllers/UserController/UserController.swift
@@ -9,12 +9,12 @@ final class UserController{
     ///用户注册
     func register(req:Request) throws -> Future<ResponseModel<UserRegistInfo>>{
         return try req.content.decode(RequestModel<UserRegistInfo>.self).flatMap(){ requestBody in 
-            return try SercretKey.JudgeSercretKey(sercreKey:requestBody.sercreKey,req:req).flatMap(){result in 
+            return try SercretKey.JudgeSercretKey(sercreKey:requestBody.sercretkey,req:req).flatMap(){result in 
                 if !result{
-                    return req.eventLoop.newSuccessdedFuture(result:ResponseModel<UserRegistInfo>(status:-3,message:secretKeyError,data:nil))
+                    return req.eventLoop.newSucceededFuture(result:ResponseModel<UserRegistInfo>(status:-3,message:secretKeyError,data:nil))
                 }
                 if requestBody.data == nil{
-                    return req.eventLoop.newSuccessdedFuture(result:ResponseModel<UserRegistInfo>(status:-2,message:paramError,data:nil))
+                    return req.eventLoop.newSucceededFuture(result:ResponseModel<UserRegistInfo>(status:-2,message:paramError,data:nil))
                 }
                 return try requestBody.data!.check(req:req).flatMap(){checkresult in
                     if checkresult == .ok{
@@ -23,7 +23,7 @@ final class UserController{
                             return ResponseModel<UserRegistInfo>(status:0,message:"注册成功",data:nil)
                         }
                     }else{
-                        return req.eventLoop.newSuccessdedFuture(result:ResponseModel<UserRegistInfo>(status:-1,message:checkresult.rawValue,data:nil))
+                        return req.eventLoop.newSucceededFuture(result:ResponseModel<UserRegistInfo>(status:-1,message:checkresult.rawValue,data:nil))
                     }
                 }
             }
