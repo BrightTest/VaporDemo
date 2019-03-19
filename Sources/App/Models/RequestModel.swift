f4d1e92333205b53529e6a9b2ab811062a92081b
BrightTest <GZSD@gzsddemacbook-air.local>
2019-03-19 16:08:10 +0800

    UserRegistInfo

--- Sources/App/Models/RequestModel.swift
+++ Sources/App/Models/RequestModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import Vapor
+import FluentMySQL
+import Crypto
+
+///保证接口安全
+struct SercretKey:MySQLModel{
+    var id:Int?
+    var sercretkey:String
+}
+
+extension SercretKey:Migration{}
+extension SercretKey{
+    //检查是否授权
+    static func JudgeSercretKey(sercreKey:String,req:Request) throws -> Future<Bool>{
+        return try SercretKey.query(on:req).filter(\.sercretkey == sercrekey).first().map(){ result in
+            if result == nil{
+                return false
+            }
+            return true
+        }
+    }
+}
--- Sources/App/Models/UserModel/UserRegisterInfo.swift
+++ Sources/App/Models/UserModel/UserRegisterInfo.swift
@@ -8,53 +8,65 @@
 import Foundation
 import Vapor
 import FluentMySQL
+import Crypto
 
-enum UserRegisterEnum:String{
+enum UserRegistEnum : String {
     case ok = "注册成功"
-    case verifycodeError = "验证码错误"
-    case useralreadyExit = "用户名已被注册"
-    case passwordisEmpty = "密码不能为空"
-    case emailisEmpty = "邮箱不能为空"
-    case verifycodeisEmpty = "验证码不能为空"
+    case verifycodeError    =    "验证码错误"
+    case useralreadyexist   =    "用户名已被注册"
+    case passwordisEmpty    =    "密码不能为空"
+    case emailisEmpty       =    "邮箱不能为空"
+    case verifycodeisEmpty  =    "验证码不能为空"
 }
 
-struct UserRegisterInfo:MySQLModel{
+
+
+
+final class UserRegistInfo : MySQLModel{
     static var entity = "user_regist_info"
-    
-    var id:Int?
-    var username:String
-    var password:String?
-    var email:String?
-    var verifycode:String?
+    var id : Int?
+    var username    :   String
+    var password    :   String?
+    var email       :   String?
+    var verifycode  :   String?
+    init(_ id : Int?, _ username : String, password : String?, _ email : String?,_ verifycode : String?) {
+        self.id = id
+        self.username = username
+        self.password = password
+        self.email    = email
+        self.verifycode = verifycode
+    }
 }
 
-extension UserRegisterInfo{
-    func check(req:Request) throws ->Future<UserRegisterEnum>{
-        ///检查数据完整性
-        guard self.password != nil else{
+extension UserRegistInfo{
+    func check(req : Request)throws -> Future<UserRegistEnum>{
+        /// 第一步，检查数据完整性
+        guard self.password != nil else {
             return req.eventLoop.newSucceededFuture(result: .passwordisEmpty)
         }
         guard let email = self.email else{
             return req.eventLoop.newSucceededFuture(result: .emailisEmpty)
         }
-        guard let verifyCode = self.verifycode else{
+        guard let verifyCode = self.verifycode else {
             return req.eventLoop.newSucceededFuture(result: .verifycodeisEmpty)
         }
-        //检查验证是否正确
-        let userMail = GBoxEmail(email:email,verifycode:verifyCode)
-        if !userMail.checkVerifyCode(){
+        
+        // 检查验证码是否正确
+        let userMail = PUBGBoxEmail(email: email, verifycode: verifyCode)
+        if !userMail.checkVerifyCode() {
             return req.eventLoop.newSucceededFuture(result: .verifycodeError)
         }
-        //检查是否注册过
-        return try UserRegisterInfo.query(on: req).filter(\UserRegisterInfo.username == self.username).first().map({ user  in
-            if user != nil{
-                return .useralreadyExit
+        // 第二步，检查是否注册过
+        return try UserRegistInfo.query(on: req).filter(\UserRegistInfo.username == self.username).first().map(){ user in
+            if user != nil {
+                return .useralreadyexist
             }
             return .ok
-        })
+        }
     }
+    
 }
 
-extension UserRegisterInfo:Migration{}
-
-extension UserRegisterInfo:Content{}
+extension UserRegistInfo : Migration { }
+/// Allows `Todo` to be encoded to and decoded from HTTP messages.
+extension UserRegistInfo: Content { }
