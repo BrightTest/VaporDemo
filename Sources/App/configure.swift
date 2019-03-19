40270dc5a437dab48adb80948c8a0cac7148727d
BrightTest <GZSD@GZSDdeMacBook-Air.local>
2019-03-19 09:17:00 +0800

    初始化

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,10 @@
-import FluentSQLite
+import FluentMySQL
 import Vapor
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     // Register providers first
-    try services.register(FluentSQLiteProvider())
+    try services.register(FluentMySQLProvider())
 
     // Register routes to the router
     let router = EngineRouter.default()
@@ -17,16 +17,14 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
 
-    // Configure a SQLite database
-    let sqlite = try SQLiteDatabase(storage: .memory)
-
-    // Register the configured SQLite database to the database config.
     var databases = DatabasesConfig()
-    databases.add(database: sqlite, as: .sqlite)
+    let mysqlConfig = MySQLDatabaseConfig(hostname: "139.199.81.222", port: 3306, username: "root", password: "123456", database: "vapor", transport: .unverifiedTLS)
+    let mysql = MySQLDatabase(config: mysqlConfig)
+    databases.add(database: mysql, as: .mysql)
     services.register(databases)
 
     // Configure migrations
-    var migrations = MigrationConfig()
-    migrations.add(model: Todo.self, database: .sqlite)
-    services.register(migrations)
+//    var migrations = MigrationConfig()
+//
+//    services.register(migrations)
 }
